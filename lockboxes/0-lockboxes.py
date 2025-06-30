db721f910b3ed60e1fc16014ed26f09c775b2369
Djamaldine <djamaldine-mohamed@live.fr>
2025-06-30 10:13:50 +0200

    Implement canUnlockAll function and main script for lockboxes problem

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+    :param boxes: list of lists, each list contains keys inside a box
+    :return: True if all boxes can be opened, else False
+    """
+    if not boxes or not isinstance(boxes, list):
+        return False
+
+    n = len(boxes)
+    opened = set([0])
+    keys = set(boxes[0])
+
+    while keys:
+        key = keys.pop()
+        if 0 <= key < n and key not in opened:
+            opened.add(key)
+            keys.update(boxes[key])
+
+    return len(opened) == n
